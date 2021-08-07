997ccdcabfaa898cb0a1274e7445622f417092cc
mhedegaardthorup <mht@netcompany.com>
2021-08-07 11:57:52 +0200

    showed proof of concept

---
+++ DWH_DB/Scripts/Data/DWHColumnData.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [DWH].[DWHColumnData]
+
+AS
+BEGIN
+	TRUNCATE TABLE [DWH].[DWHColumns]
+
+	INSERT INTO [DWH].[DWHColumns](SchemaName, TableName, ColumnName, DataType, IsNullable, ColProperty) VALUES
+	('Extract','','DWH_ID','BIGINT','NOT NULL','IDENTITY (1, 1)'),
+	('Extract','','DWH_CreateJobID','BIGINT','NOT NULL','')
+END
